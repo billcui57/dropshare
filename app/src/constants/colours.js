0b502809956ef89a6dd241024155818294d2716c
Bill Cui <billc@kritik.io>
2021-08-15 21:23:33 -0400

    new files

---
+++ app/src/constants/colours.js
@@ -0,0 +1,8 @@
+export const BLUE = "blue-500";
+export const BLUE_DARK = "blue-600";
+export const RED = "red-400";
+export const RED_DARK = "red-500";
+export const RED_DARKER = "red-600";
+export const YELLOW = "yellow-400";
+export const TEXT = "black";
+export const WHITE = "white";
