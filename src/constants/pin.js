6064de864db5bad3ca2fc76c9d74cf655fa6410a
Bill Cui <billcui2001@hotmail.com>
2021-08-22 13:25:57 -0400

    oops

---
+++ src/constants/pin.js
@@ -0,0 +1,13 @@
+export const CATEGORIES = ["Food"];
+
+const FOOD_SUBCATEGORIES = [
+  "Vegetables",
+  "Fruits",
+  "Grains, legumes, nuts, and seeds",
+  "Meat and poultry",
+  "Fish and seafood",
+  "Dairy foods",
+  "Eggs",
+];
+
+export const SUBCATEGORIES = [...FOOD_SUBCATEGORIES];
