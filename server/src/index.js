b1f8e6df6cb671628e03cecd077417e8ee00a045
Bill Cui <billcui2001@hotmail.com>
2021-09-17 11:07:19 -0400

    production server config

--- server/src/index.js
+++ server/src/index.js
@@ -6,12 +6,12 @@ import cors from "cors";
 import "./config/mongodb.config";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.SERVER_PORT;
 
 app.use(express.json({ limit: "10MB" }));
 app.use(cors());
 
-app.use("/api/v1", Routes.initPrivateRoutes());
+app.use(Routes.initPrivateRoutes());
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
